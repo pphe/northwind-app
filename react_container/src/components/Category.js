23b5435cb27bbcb56efe21f1425265329b19ba2b
Peter Phe <peterphe@gmail.com>
2018-10-22 12:05:46 -0700

    Add simple table to display data in Category component

--- react_container/src/components/Category.js
+++ react_container/src/components/Category.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+import axios from 'axios';
 
 class Category extends React.Component {
     constructor(props) {
@@ -6,9 +8,39 @@ class Category extends React.Component {
         this.state = { data: [] };
     }
 
+    componentDidMount() {
+        axios.get('/api/category')
+            .then(res => this.setState({ data: res.data }))
+            .catch(err => console.log(`Err: ${err}`));
+    }
+
     render() {
+        const categories = this.state.data
+            .slice() // a copy so we don't mutate the original data
+            .sort((a,b) => a.CategoryID <= b.CategoryID ? -1 : 1)
+            .map(category => {
+                return (
+                    <tr key={category.CategoryID}>
+                        <td>{category.CategoryID}</td>
+                        <td>{category.CategoryName}</td>
+                        <td>{category.Description}</td>
+                    </tr>
+                )
+            });
+
         return (
-            <div>category component</div>
+            <Table bordered>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {categories}
+                </tbody>
+            </Table>
         );
     }
 }
