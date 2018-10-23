ad57084865ca5a102c63560cf608ab278f13f562
Peter Phe <peterphe@gmail.com>
2018-10-22 19:13:12 -0700

    Remove unnecessary dataService files
    Sticking with individual service calls for each model rather than aggregating and exposing as a separate service.

--- node_container/routes/dataService.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const service = require('../services/dataService');
-
-router.use('/:orderId([0-9]+)', service.getOrderDetailsByOrderId);
-
-module.exports = router;
